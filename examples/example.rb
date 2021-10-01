497aa5311e51c437bd731e8a7faf8ffb38187310
Lassi Kortela <lassi@lassi.io>
2021-10-01 12:45:52 +0300

    Add error capture to Ruby example

--- examples/example.rb
+++ examples/example.rb
@@ -1,27 +1,32 @@
 #! /usr/bin/env ruby
 
+require "json"
+require "open3"
 require "rubygems/package"
 
 def pandoc(from_format, to_format, strings)
-  cmd = ["pandoc-tar", "--from", from_format, "--to", to_format]
-  IO.popen(cmd, "w+") do |subprocess|
-    Gem::Package::TarWriter.new(subprocess) do |tar|
+  cmd = ["pandoc-tar", "--from", from_format, "--to", to_format, "--log", "-"]
+  Open3.popen3(*cmd) do |sub_in, sub_out, sub_err, sub_wait|
+    Gem::Package::TarWriter.new(sub_in) do |tar|
       strings.each_with_index do |string, i|
         tar.add_file_simple("#{i}", 0, string.length) do |output|
           output.write(string)
         end
       end
     end
-    subprocess.close_write
+    sub_in.close_write
     results = []
-    Gem::Package::TarReader.new(StringIO.new(subprocess.read)) do |tar|
+    Gem::Package::TarReader.new(StringIO.new(sub_out.read)) do |tar|
       tar.each do |entry|
         results.push(if entry.file? then entry.read else nil end)
       end
     end
-    results
+    errors = JSON.load(sub_err)
+    [results, errors]
   end
 end
 
 MARKDOWN = ["# Hello world", "* foo\n* bar"]
-pandoc("markdown", "html", MARKDOWN).each do |s| puts s end
+results, errors = pandoc("markdown", "html", MARKDOWN)
+$stderr.puts(errors.inspect)
+results.each do |s| puts s end
